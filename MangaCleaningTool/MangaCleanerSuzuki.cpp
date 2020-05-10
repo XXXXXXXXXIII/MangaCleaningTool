b0cd0fab1fa832ac2729c05881029a3576d61a0b
XXXXXXXXXIII <tim.xie.2009@gmail.com>
2020-05-10 16:03:45 -0400

    Added opencv library

---
+++ MangaCleaningTool/MangaCleanerSuzuki.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    waitKey(0);
+}
