fd189958b8f353a8947c7b9317237be2aa3de0e1
XXXXXXXXXIII <tim.xie.2009@gmail.com>
2020-11-06 23:26:33 -0500

    Added simple cmd arguments

--- MangaCleaningTool/MangaCleanerSuzuki.cpp
+++ MangaCleaningTool/MangaCleanerSuzuki.cpp
@@ -10,7 +10,7 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/ml.hpp>
 #include <opencv2/features2d.hpp>
-#include <tesseract/baseapi.h>
+//#include <tesseract/baseapi.h>
 
 #include "psd_sdk/Psd.h"
 #include "psd_sdk/PsdPlatform.h"
@@ -23,15 +23,34 @@ using namespace std;
 using namespace cv;
 using namespace cv::ml;
 using namespace mct;
-namespace ts = tesseract;
+//namespace ts = tesseract;
 
-int main()
+int main(int argc, char** args)
 {
+    bool doClean = false, doFrame = false, doShow = false;
+    if (argc > 1)
+    {
+        if (string(args[1]).find('c') != string::npos)
+        {
+            doClean = true;
+        }
+        if (string(args[1]).find('f') != string::npos)
+        {
+            doFrame = true;
+        }
+        if (string(args[1]).find('s') != string::npos)
+        {
+            doShow = true;
+        }
+    }
+
+
     map<wstring, Mat> images = loadImages(selectImageFromDialog());
     Ptr<BoostFrameClassifier> frame_boost = new BoostFrameClassifier();
     Ptr<BoostBubbleClassifier> bubble_boost = new BoostBubbleClassifier();
-    Ptr<TesseractTextDetector> tess = new TesseractTextDetector(); // Inefficient
+    //Ptr<TesseractTextDetector> tess = new TesseractTextDetector(); // Inefficient
     Ptr<MSERTextDetector> text_mser = new MSERTextDetector(); // Need filtering
+    //Ptr<TFBubbleClassifier> bubble_tf = new TFBubbleClassifier();
     //Ptr<MSERBubbleDetector> bubble_mser = new MSERBubbleDetector();
     //Ptr<EASTTextDetector> east = new EASTTextDetector(); // Poor accuracy
 
@@ -42,12 +61,24 @@ int main()
 
         printf("Width: %d   Height: %d   \n", img.second.cols, img.second.rows);
 
-        cleanImage(img_gray);
 
         auto timer = startTimer();
-        vector<Frame> frames = extractFrame(img_gray);
-        frame_boost->classifyFrame(frames);
-        straightenImage(img_gray, frames);
+        if (doClean)
+        {
+            cleanImage(img_gray);
+        }
+
+        vector<Frame> frames;
+        if (doFrame)
+        {
+            frames = extractFrame(img_gray);
+            frame_boost->classifyFrame(frames);
+        }
+        //straightenImage(img_gray, frames);
+
+        //frames = extractFrame(img_gray);
+        //frame_boost->classifyFrame(frames);
+        //Mat img_frameless = img_gray.clone();
         //cleanFrame(img_gray, frames); //TODO: Not very working
         cout << "Frame: " << stopTimer(timer) << endl;
 
@@ -63,29 +94,39 @@ int main()
         cout << "Text: " << stopTimer(timer) << endl;
 
         timer = startTimer();
+        //bubble_tf->classifyBubble(img_gray, bubbles);
         bubble_boost->classifyBubble(bubbles);
         cout << "Bubble Classifier: " << stopTimer(timer) << endl;
 
-        cleanBubble(img_gray, bubbles);
-        //showImage(img_gray, "image", 0.5);
-
+        Mat img_out = img_gray.clone();
+        cleanBubble(img_out, bubbles);
+        if (doFrame)
+        {
+            cleanFrame(img_out, frames);
+        }
 
         Mat img_mask(img.second.size(), CV_8UC2);
         vector<Mat> ch_mask;
         split(img_mask, ch_mask);
-        bitwise_or(createBubbleMask(img.second.size(), bubbles, 0, 255),
-            createFrameMask(img.second.size(), frames, 255, 0), ch_mask[0]);
+        ch_mask[0] = createBubbleMask(img.second.size(), bubbles, 0, 255);
+        //bitwise_or(createBubbleMask(img.second.size(), bubbles, 0, 255), createFrameMask(img.second.size(), frames, 255, 0), ch_mask[0]);
         bitwise_or(ch_mask[0], ch_mask[1], ch_mask[1]);
         merge(ch_mask, img_mask);
 
-        saveImage(img_gray, img.first);
+        if (doShow)
+        {
+            showImage(img_out, "image");
+            //saveImage(img_out, img.first);
+        }
+        else
+        {
+            psd::ExportDocument* psd_doc = createPsdDocument(img.second.size(), CV_8UC1);
+            //addPsdLayer(psd_doc, img.second, "Original");
+            addPsdLayer(psd_doc, img_gray, "Clean");
+            addPsdLayer(psd_doc, img_mask, "Mask");
+            savePsdDocument(psd_doc, img.first);
+        }
 
-        //psd::ExportDocument* psd_doc = createPsdDocument(img.second.size(), CV_8UC3);
-        //addPsdLayer(psd_doc, img.second, "Original");
-        //addPsdLayer(psd_doc, img_gray, "Level");
-        //addPsdLayer(psd_doc, img_mask, "Mask");
-        //savePsdDocument(psd_doc, img.first);
-        //writePSD(toGrayScale(img.second), img_mask, img.first);
     }
 
     cv::waitKey(0);
